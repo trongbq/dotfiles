9e2689cc65a601fd48867231ccf47afc77a76eb5
Trong Bui <trong.buiquoc@gmail.com>
2017-01-05 17:22:38 +0700

    Add changes to install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,19 @@
-# install zsh config
-cat zsh > ~/.zsh
-
 # install tmux config file
 cat tmux.conf > ~/.tmux.conf
 
 # install gitconfig file
 cat gitconfig > ~/.gitconfig
+
+# install vim config
+cat vimrc > ~/.vimrc
+
+mkdir -p ~/.vim/bundle
+mkdir -p ~/.vim/autoload
+
+cd ~/.vim/bundle
+git clone https://github.com/scrooloose/nerdtree.git
+
+sudo apt-get install exuberant-ctags
+cd ~/.vim/bundle
+git clone git://github.com/majutsushi/tagbar
 
