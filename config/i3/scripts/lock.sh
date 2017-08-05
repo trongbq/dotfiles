4a758502eb6e8fbd8688093b30f3d3b61863e0ab
Trong Bui <trong.buiquoc@gmail.com>
2017-08-06 00:48:26 +0700

    Add lock to i3

--- config/i3/scripts/lock.sh
+++ config/i3/scripts/lock.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
 
-icon="$HOME/.config/i3/scripts/lock.png"
-tmpbg='/tmp/screen.png'
+# get path where the script is located to find the lock icon
+script_path=$(readlink -f -- "$0")
+script_path=${script_path%/*}
 
+echo "$script_path"
+#icon="$script_path/icons/lock.png"
+tmpbg="/tmp/screen.png"
+echo "$icons"
 (( $# )) && { icon=$1; }
 
 scrot "$tmpbg"
 convert "$tmpbg" -scale 10% -scale 1000% "$tmpbg"
-convert "$tmpbg" "$icon" -gravity center -composite -matte "$tmpbg"
-i3lock -u -i "$tmpbg
+#convert "$tmpbg" "$icon" -gravity center -composite -matte "$tmpbg"
+i3lock -i "$tmpbg"
