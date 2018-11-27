34bd73b512bef499a76a807778a5cf330ea93c78
Bui Quoc Trong <trongbq@users.noreply.github.com>
2018-11-27 13:15:24 +0700

    Update init.el

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,40 +1,278 @@
-;;; init.el
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (spaceline company-lsp company lsp-ui lsp-mode flycheck highlight-indent-guides vue-mode lsp-javascript-typescript tern js2-mode helm-projectile neotree projectile general which-key use-package helm evil-escape evil doom-themes))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
 
+;; Minimal UI
+(scroll-bar-mode -1)
+(tool-bar-mode   -1)
+(tooltip-mode    -1)
+(menu-bar-mode   -1)
+(global-linum-mode 1)
+(add-to-list 'default-frame-alist '(font . "Inconsolata-13"))
+(add-to-list 'default-frame-alist '(height . 24))
+(add-to-list 'default-frame-alist '(width . 80))
+
+;; Package configs
+(require 'package)
+(setq package-enable-at-startup nil)
+(setq package-archives '(("org"   . "http://orgmode.org/elpa/")
+                         ("gnu"   . "http://elpa.gnu.org/packages/")
+                         ("melpa" . "https://melpa.org/packages/")))
 (package-initialize)
 
-;; Keep automated customizations in a separated file
-(setq custom-file "~/.emacs.d/personal/custom.el")
-(load custom-file 'noerror)
+;; Bootstrap `use-package`
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(require 'use-package)
 
-(add-to-list 'load-path (expand-file-name "personal" user-emacs-directory))
-(add-to-list 'load-path (expand-file-name "extentions" user-emacs-directory))
+;; Vim mode
+(use-package evil
+  :ensure t
+  :config
+  (evil-mode 1))
 
-(let ((files (directory-files-and-attributes "~/.emacs.d/personal" t)))
-  (dolist (file files)
-    (let ((filename (car file))
-	  (dir (nth 1 file)))
-      (when (and dir
-		 (not (string-suffix-p "." filename)))
-	(add-to-list 'load-path (car file))))))
+(use-package evil-escape
+  :ensure t
+  :init
+  (setq-default evil-escape-key-sequence "jk")
+  :config
+  (evil-escape-mode 1))
 
-(let ((files (directory-files-and-attributes "~/.emacs.d/extentions" t)))
-  (dolist (file files)
-    (let ((filename (car file))
-	  (dir (nth 1 file)))
-      (when (and dir
-		 (not (string-suffix-p "." filename)))
-	(add-to-list 'load-path (car file))))))
+;; Theme
+(use-package doom-themes
+  :ensure t
+  :config
+  (load-theme 'doom-one t))
 
-(add-to-list 'custom-theme-load-path (expand-file-name "themes" user-emacs-directory))
+;; Helm
+(use-package helm
+  :ensure t
+  :init
+  (setq helm-M-x-fuzzy-match t
+  helm-mode-fuzzy-match t
+  helm-buffers-fuzzy-matching t
+  helm-recentf-fuzzy-match t
+  helm-locate-fuzzy-match t
+  helm-semantic-fuzzy-match t
+  helm-imenu-fuzzy-match t
+  helm-completion-in-region-fuzzy-match t
+  helm-candidate-number-list 150
+  helm-split-window-in-side-p t
+  helm-move-to-line-cycle-in-source t
+  helm-echo-input-in-header-line t
+  helm-autoresize-max-height 0
+  helm-autoresize-min-height 20)
+  :config
+  (helm-mode 1))
 
-(require 'defuns)
-(require 'init-global)
-(require 'init-elpa)
-(require 'init-font)
-(require 'init-platform)
-(require 'init-package)
-(require 'highlight-indentation)
+;; Projectile
+(use-package projectile
+  :ensure t
+  :init
+  (setq projectile-require-project-root nil)
+  :config
+  (projectile-mode 1))
 
-(add-hook 'python-mode-hook 'highlight-indentation-mode)
-(add-hook 'ruby-mode-hook 'highlight-indentation-mode)
-(add-hook 'js-mode-hook 'highlight-indentation-mode)
+;; Helm Projectile
+(use-package helm-projectile
+  :ensure t
+  :init
+  (setq helm-projectile-fuzzy-match t)
+  :config
+  (helm-projectile-on))
+
+;; Which Key
+(use-package which-key
+  :ensure t
+  :init
+  (setq which-key-separator " ")
+  (setq which-key-prefix-prefix "+")
+  :config
+  (which-key-mode 1))
+
+;; Custom keybinding
+
+(global-set-key [f8] 'neotree-toggle)
+
+(use-package general
+  :ensure t
+  :config (general-define-key
+  :states '(normal visual insert emacs)
+  :prefix "SPC"
+  :non-normal-prefix "M-SPC"
+  "/"   '(helm-projectile-rg :which-key "ripgrep")
+  "TAB" '(switch-to-prev-buffer :which-key "previous buffer")
+  "SPC" '(helm-M-x :which-key "M-x")
+  "pf"  '(helm-projectile-find-file :which-key "find files")
+  "pp"  '(helm-projectile-switch-project :which-key "switch project")
+  "pb"  '(helm-projectile-switch-to-buffer :which-key "switch buffer")
+  "pr"  '(helm-show-kill-ring :which-key "show kill ring")
+  ;; Buffers
+  "bb"  '(helm-mini :which-key "buffers list")
+  ;; Window
+  "wl"  '(windmove-right :which-key "move right")
+  "wh"  '(windmove-left :which-key "move left")
+  "wk"  '(windmove-up :which-key "move up")
+  "wj"  '(windmove-down :which-key "move bottom")
+  "w/"  '(split-window-right :which-key "split right")
+  "w-"  '(split-window-below :which-key "split bottom")
+  "wx"  '(delete-window :which-key "delete window")
+  "qz"  '(delete-frame :which-key "delete frame")
+  "qq"  '(kill-emacs :which-key "quit")
+  ;; NeoTree
+  "ft"  '(neotree-toggle :which-key "toggle neotree")
+  ;; Others
+  "at"  '(ansi-term :which-key "open terminal")
+))
+
+;; Fancy titlebar for MacOS
+(add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
+(add-to-list 'default-frame-alist '(ns-appearance . dark))
+(setq ns-use-proxy-icon  nil)
+(setq frame-title-format nil)
+
+;; All The Icons: M-x all-the-icons-install-fonts
+(use-package all-the-icons :ensure t)
+
+;; NeoTree
+(use-package neotree
+  :ensure t
+  :init
+  (setq neo-theme (if (display-graphic-p) 'icons 'arrow)))
+
+;; Show matching parens
+(setq show-paren-delay 0)
+(show-paren-mode 1)
+
+;; Disable backup files
+(setq make-backup-files nil) ; stop creating backup~ files
+(setq auto-save-default nil) ; stop creating #autosave# files
+
+;; Load bash file manually
+(let ((path (shell-command-to-string ". ~/.bashrc; echo -n $PATH")))
+  (setenv "PATH" path)
+  (setq exec-path 
+        (append
+         (split-string-and-unquote path ":")
+         exec-path)))
+
+;; Splash Screen
+(setq inhibit-startup-screen t)
+(setq initial-scratch-message ";; Happy Hacking")
+
+;; Paragraph movement
+(global-set-key (kbd "s-j") 'forward-paragraph)
+(global-set-key (kbd "s-k") 'backward-paragraph)
+
+;; Flycheck
+(use-package flycheck
+  :ensure t
+  :init (global-flycheck-mode))
+
+;; LSP
+(use-package lsp-mode
+  :ensure t
+  :init
+  (add-hook 'prog-major-mode #'lsp-prog-major-mode-enable))
+
+(use-package lsp-ui
+  :ensure t
+  :init
+  (add-hook 'lsp-mode-hook 'lsp-ui-mode))
+
+;; Company mode
+(use-package company
+:ensure t
+:init
+(setq company-minimum-prefix-length 3)
+(setq company-auto-complete nil)
+(setq company-idle-delay 0)
+(setq company-require-match 'never)
+(setq company-frontends
+  '(company-pseudo-tooltip-unless-just-one-frontend
+    company-preview-frontend
+    company-echo-metadata-frontend))
+(setq tab-always-indent 'complete)
+(defvar completion-at-point-functions-saved nil)
+:config
+(global-company-mode 1)
+(define-key company-active-map (kbd "TAB") 'company-complete-common-or-cycle)
+(define-key company-active-map (kbd "<tab>") 'company-complete-common-or-cycle)
+(define-key company-active-map (kbd "S-TAB") 'company-select-previous)
+(define-key company-active-map (kbd "<backtab>") 'company-select-previous)
+(define-key company-mode-map [remap indent-for-tab-command] 'company-indent-for-tab-command)
+(defun company-indent-for-tab-command (&optional arg)
+  (interactive "P")
+  (let ((completion-at-point-functions-saved completion-at-point-functions)
+    	(completion-at-point-functions '(company-complete-common-wrapper)))
+	(indent-for-tab-command arg)))
+
+(defun company-complete-common-wrapper ()
+	(let ((completion-at-point-functions completion-at-point-functions-saved))
+	(company-complete-common))))
+
+(use-package company-lsp
+:ensure t
+:init
+(push 'company-lsp company-backends))
+
+;; Powerline
+(use-package spaceline
+  :ensure t
+  :init
+  (setq powerline-default-separator 'slant)
+  :config
+  (spaceline-emacs-theme)
+  (spaceline-toggle-minor-modes-off)
+  (spaceline-toggle-buffer-size-off)
+  (spaceline-toggle-evil-state-on))
+
+(use-package highlight-indent-guides
+  :ensure t
+  :init
+  (add-hook 'prog-mode-hook 'highlight-indent-guides-mode))
+
+(use-package org
+  :mode (("\\.org$" . org-mode))
+  :ensure org-plus-contrib
+  :config
+  (progn
+    ;; config stuff
+    ))
+
+;;;;;;;;;;;;;;;;;;;;;;;
+;; Language Supports ;;
+;;;;;;;;;;;;;;;;;;;;;;;
+
+;; JavaScript
+(use-package js2-mode
+  :ensure t
+  :init
+  (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode)))
+(use-package tern :ensure t)
+
+(use-package lsp-javascript-typescript
+  :ensure t
+  :init
+  (add-hook 'js-mode-hook #'lsp-javascript-typescript-enable))
+
+(use-package vue-mode
+  :ensure t
+  :init)
+
+(use-package haskell-mode
+  :mode (("\\.hs$" . haskell-mode))
+  :ensure t
+  :init)
